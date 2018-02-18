8c6c90c4683edfded520a1176feec63d2f77ffb4
royl8 <royl8@qq.com>
2018-02-17 21:59:48 -0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
-# assume in python virtual environment
+source env/bin/activate
+
+pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
-sudo env/bin/python manage.py collectstatic
+sudo env/bin/python manage.py collectstatic --noinput
 
 sudo service memcached restart
 sudo service apache2 restart
