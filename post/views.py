36f0f9edf44e74262eb62c1a8f1fb86c3decd381
royl8 <royl8@qq.com>
2018-03-01 22:47:48 -0800

    enable uploading a single html file (no need to wrap it into a zip file to upload)

--- post/views.py
+++ post/views.py
@@ -9,95 +9,115 @@ from services import (extract_uploaded_zip_file_into_post_folder,
 
 
 def display_post_index(request):
-    if "tag_id" in request.GET:
-        tag_chosen = Tag.objects.get(id=int(request.GET["tag_id"]))
+    if 'tag_id' in request.GET:
+        tag_chosen = Tag.objects.get(id=int(request.GET['tag_id']))
         context = {
-            "tag_chosen": tag_chosen,
-            "posts": tag_chosen.linked_posts.all(),
-            "tags": Tag.objects.all(),
+            'tag_chosen': tag_chosen,
+            'posts': tag_chosen.linked_posts.all(),
+            'tags': Tag.objects.all(),
         }
     else:
         context = {
-            "posts": Post.objects.all().order_by('-post_time'),
-            "tags": Tag.objects.all(),
-            "nav_and_sidebar_animated": True
+            'posts': Post.objects.all().order_by('-post_time'),
+            'tags': Tag.objects.all(),
+            'nav_and_sidebar_animated': True
         }
-    return render(request, "post/post_index.html", context)
+    return render(request, 'post/post_index.html', context)
 
 
 def display_post(request):
-    post = Post.objects.get(id=int(request.GET["post_id"]))
+    post = Post.objects.get(id=int(request.GET['post_id']))
     post.num_view += 1
     post.save()
     context = {
-        "post": post,
-        "tags": Tag.objects.all(),
-        "nav_and_sidebar_animated": True
+        'post': post,
+        'tags': Tag.objects.all(),
+        'nav_and_sidebar_animated': True
     }
-    return render(request, "post/post_page.html", context)
+    return render(request, 'post/post_page.html', context)
 
 
-def post_zip_handler(post, zip_uploaded):
+def _post_zip_handler(post, zip_uploaded):
     # create the correct folder and move content inside
     post_folder_path = os.path.join(settings.MEDIA_ROOT, str(post.id))
     remove_old_version_post_folder_on_disk_if_exists(post_folder_path)
     os.mkdir(post_folder_path)  # create a new folder
     extract_uploaded_zip_file_into_post_folder(zip_uploaded, post_folder_path)
 
-    # create a new html file named "index.html", add "/media/[post.pk]/" in front of reference to the static file
+    # create a new html file named 'index.html', add '/media/[post.pk]/' in front of reference to the static file
     for file in os.listdir(post_folder_path):
-        if file.endswith(".htm") or file.endswith(".html"):
-            file_name = file.split(".")[0]
-            index = open(os.path.join(post_folder_path, "index.htm"), 'wb+')
+        if file.endswith('.htm') or file.endswith('.html'):
+            file_name = file.split('.')[0]
+            index = open(os.path.join(post_folder_path, 'index.htm'), 'wb+')
             original_index = open(os.path.join(post_folder_path, file), 'r')
             for line in original_index:
-                line = line.replace(file_name + ".file",
-                                    '/media/' + str(post.pk) + "/" + file_name + ".file")
-                line = line.replace(file_name + ".fld",
-                                    '/media/' + str(post.pk) + "/" + file_name + ".fld")
+                line = line.replace(file_name + '.file',
+                                    '/media/' + str(post.pk) + '/' + file_name + '.file')
+                line = line.replace(file_name + '.fld',
+                                    '/media/' + str(post.pk) + '/' + file_name + '.fld')
                 # restrict style of the anchor tag only within the post content part
                 # provent the style to influence other parts of the page
-                # line = line.replace("a:visited", "#content_from_file a:visited").replace("a:link", "#content_from_file a:link")
+                # line = line.replace('a:visited', '#content_from_file a:visited').replace('a:link', '#content_from_file a:link')
                 index.write(line)
             original_index.close()
             index.close()
 
+def _post_html_handler(post, html_uploaded):
+    post_folder_path = os.path.join(settings.MEDIA_ROOT, str(post.id))
+    remove_old_version_post_folder_on_disk_if_exists(post_folder_path)
+    os.mkdir(post_folder_path)
+
+    html_on_disk_path = os.path.join(post_folder_path, 'index.htm')
+    html_on_disk = open(html_on_disk_path, 'wb+')
+    for chunk in html_uploaded.chunks():
+        html_on_disk.write(chunk)
+    html_on_disk.close()
 
 def upload_post(request):
     if not request.user.is_staff:
         return HttpResponse()
-    post = Post(title=request.POST["title"])
+    post = Post(title=request.POST['title'])
     post.save()
-    # request.FILES["zip_uploaded"] is an UploadedFile object
-    post_zip_handler(post, request.FILES["zip_uploaded"])
-    return redirect("/post/?post_id=" + str(post.pk))
+
+    # request.FILES['file_uploaded'] is an UploadedFile object
+    uploaded_file = request.FILES['file_uploaded']
+    if uploaded_file.name.endswith('html') or uploaded_file.name.endswith('htm'):
+        _post_html_handler(post, uploaded_file)
+    else:
+        _post_zip_handler(post, uploaded_file)
+    return redirect('/post/?post_id=' + str(post.pk))
 
 
 def update_post(request):
     if not request.user.is_staff:
         return HttpResponse()
-    post = Post.objects.get(id=int(request.POST["post_id"]))
-    post.title = request.POST["title"]
+    post = Post.objects.get(id=int(request.POST['post_id']))
+    post.title = request.POST['title']
     post.save()
-    if "zip_uploaded" in request.FILES:
-        post_zip_handler(post, request.FILES["zip_uploaded"])
-    return redirect("/post/?post_id=" + str(post.pk))
+
+    if 'file_uploaded' in request.FILES:
+        uploaded_file = request.FILES['file_uploaded']
+        if uploaded_file.name.endswith('html') or uploaded_file.name.endswith('htm'):
+            _post_html_handler(post, uploaded_file)
+        else:
+            _post_zip_handler(post, uploaded_file)
+    return redirect('/post/?post_id=' + str(post.pk))
 
 
 def display_upload_page(request):
-    return render(request, "post/upload_post_page.html")
+    return render(request, 'post/upload_post_page.html')
 
 
 def search_post(request):
     # search post by title
-    search_key = request.GET["search_key"]
+    search_key = request.GET['search_key']
     search_result_posts = Post.objects.filter(title__icontains=search_key)  # case-insensitive contain
     context = {
-        "tags": Tag.objects.all(),
-        "posts": search_result_posts,
-        "search_key": search_key,
+        'tags': Tag.objects.all(),
+        'posts': search_result_posts,
+        'search_key': search_key,
     }
-    return render(request, "post/post_index.html", context)
+    return render(request, 'post/post_index.html', context)
 
 
 def display_raw(request, pk):
